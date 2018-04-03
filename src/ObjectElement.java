e9e0fdcd4f91e92abb81e1f6e6a9c97f1cbc8d69
Mithun <mithun@vijayasekar.com>
2018-04-03 11:52:22 -0700

    cleaned up extra tomcat, src, bin and html files

--- src/ObjectElement.java
+++
@@ -1,7 +0,0 @@
-
-public class ObjectElement extends TripleElement{
-
-	public ObjectElement(String name) {
-		super(name);
-	}
-}
--- src/Predicate.java
+++
@@ -1,6 +0,0 @@
-
-public class Predicate extends TripleElement{
-	public Predicate(String name) {
-		super(name);
-	}
-}
