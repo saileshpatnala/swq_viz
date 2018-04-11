822d031dc927c9c4ea535fecacc7256a47ff5244
wonheep <wonheeprk@gmail.com>
2018-04-10 19:47:16 -0700

    finished data cleaning script, uploaded fixed jsonld files of all library data

---
+++ data/README.md
@@ -0,0 +1,8 @@
+#Data Cleaning files from library catalog
+
+##Running script
+* Run `python3 clean_data.py`. The script will go through all .jsonld files in directory where script is saved and output a new file <filename>_fixed.jsonld with the cleaned data.
+
+##Meta information
+* strict_query file has 27,452 triples | 4.5 MB
+* loose_query file has 396,723 triples | 53.0 MB
--- data/clean_data.py
+++ data/clean_data.py
@@ -11,44 +11,31 @@ def get_basename(filename, current_path):
 	name = os.path.splitext(base)[0]
 	return name
 
-def clean_Files(new_filename):
-	# BLOCK_SIZE = 1 << 15
-	# with open(new_filename, 'rb') as fin, open('temp2.jsonld', 'wb') as fout:
-	# 	for block in iter(lambda: fin.read(BLOCK_SIZE), b''):
-	# 		block.replace('"subjects"', '"subject"')
-	# 		fout.write(block)
-	mesh = open(new_filename, "r")
-	with open("temp2.jsonld", "w") as f:
-		for line in mesh:
+def clean_Files(old_filename, new_filename):
+
+	file = open(old_filename, 'r')
+
+	with open(new_filename, 'w') as output_file:
+		for line in file:
 			line = line.replace('"subjects"', '"subject"').replace('"linked_data": {', '').replace('}},', '},').replace(', }', '}').replace('Visual material', 'Visual%20material').replace('Computer file', 'Computer%20file').replace('Mixed material', 'Mixed%20material').replace('"https://open-na.hosted.exlibrisgroup.com/alma/contexts/bib"','{"dc": "http://purl.org/dc/elements/1.1/", "bibo": "http://purl.org/ontology/bibo/", "xsd": "http://www.w3.org/2001/XMLSchema#", "dct":"http://purl.org/dc/terms/", "foaf": "http://xmlns.com/foaf/0.1/", "owl": "http://www.w3.org/2002/07/owl#", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "title": "dct:title", "creator": {"@id":"dct:creator", "@container": "@set"}, "contributor": {"@id":"dct:contributor", "@container":  "@set"}, "alternative": "dct:alternative", "publisher": {"@id":"dct:publisher", "@container": "@set"}, "language": {"@id":"dct:language", "@type":"@id"}, "subject": {"@id":"dct:subject", "@container": "@set"}, "identifier": {"@id": "dc:identifier", "@container": "@set"}, "description": "dct:description", "date": "dct:date", "medium": "dct:medium ", "format": "dct:format", "type": "dct:type", "place_of_publication": "dct:Location", "sameAs": {"@id": "owl:sameAs","@type": "@id","@container": "@set"}, "label": "rdfs:label", "edition": "bibo:edition", "note": "bibo:Note", "series": "bibo:series", "volume": "bibo:volume", "isbn": "bibo:isbn", "isbn10": "bibo:isbn10", "isbn13": "bibo:isbn13",	"oclcnum": "bibo:oclcnum", "issn": "bibo:issn", "issue": "bibo:issue"}')
-			f.write(line)
-	f.close()
+			output_file.write(line)
+			output_file.seek(0,2)
+			size = output_file.tell()
+			output_file.truncate(size-2)
+			output_file.seek(0,2)
+			output_file.write(']')
 
-	# replacement(new_filename, 'Visual material', 'Visual%20material')
-	# replacement(new_filename, 'Computer file', 'Computer%20file')
-	# replacement(new_filename, '"linked_data": {', '')
-	# replacement(new_filename, '}},', '},')
-	# replacement(new_filename, ', }', '}')
-	#replacement(new_filename, 'https://open-na.hosted.exlibrisgroup.com/alma/contexts/bib','{"dc": "http://purl.org/dc/elements/1.1/", "bibo": "http://purl.org/ontology/bibo/", "xsd": "http://www.w3.org/2001/XMLSchema#", "dct":"http://purl.org/dc/terms/", "foaf": "http://xmlns.com/foaf/0.1/", "owl": "http://www.w3.org/2002/07/owl#", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "title": "dct:title", "creator": {"@id":"dct:creator", "@container": "@set"}, "contributor": {"@id":"dct:contributor", "@container":  "@set"}, "alternative": "dct:alternative", "publisher": {"@id":"dct:publisher", "@container": "@set"}, "language": {"@id":"dct:language", "@type":"@id"}, "subject": {"@id":"dct:subject", "@container": "@set"}, "identifier": {"@id": "dc:identifier", "@container": "@set"}, "description": "dct:description", "date": "dct:date", "medium": "dct:medium ", "format": "dct:format", "type": "dct:type", "place_of_publication": "dct:Location", "sameAs": {"@id": "owl:sameAs","@type": "@id","@container": "@set"}, "label": "rdfs:label", "edition": "bibo:edition", "note": "bibo:Note", "series": "bibo:series", "volume": "bibo:volume", "isbn": "bibo:isbn", "isbn10": "bibo:isbn10", "isbn13": "bibo:isbn13",	"oclcnum": "bibo:oclcnum", "issn": "bibo:issn", "issue": "bibo:issue"}')
-
-	# replacement(new_filename, '"subjects"', '"subject"')
-	# replacement(new_filename, 'Mixed material', 'Mixed%20material')
-	# replacement(new_filename, 'Visual material', 'Visual%20material')
-	# replacement(new_filename, 'Computer file', 'Computer%20file')
-	# replacement(new_filename, '"linked_data": {', '')
-	# replacement(new_filename, '}},', '},')
-	# replacement(new_filename, ', }', '}')
-	#replacement(new_filename, 'https://open-na.hosted.exlibrisgroup.com/alma/contexts/bib','{"dc": "http://purl.org/dc/elements/1.1/", "bibo": "http://purl.org/ontology/bibo/", "xsd": "http://www.w3.org/2001/XMLSchema#", "dct":"http://purl.org/dc/terms/", "foaf": "http://xmlns.com/foaf/0.1/", "owl": "http://www.w3.org/2002/07/owl#", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "title": "dct:title", "creator": {"@id":"dct:creator", "@container": "@set"}, "contributor": {"@id":"dct:contributor", "@container":  "@set"}, "alternative": "dct:alternative", "publisher": {"@id":"dct:publisher", "@container": "@set"}, "language": {"@id":"dct:language", "@type":"@id"}, "subject": {"@id":"dct:subject", "@container": "@set"}, "identifier": {"@id": "dc:identifier", "@container": "@set"}, "description": "dct:description", "date": "dct:date", "medium": "dct:medium ", "format": "dct:format", "type": "dct:type", "place_of_publication": "dct:Location", "sameAs": {"@id": "owl:sameAs","@type": "@id","@container": "@set"}, "label": "rdfs:label", "edition": "bibo:edition", "note": "bibo:Note", "series": "bibo:series", "volume": "bibo:volume", "isbn": "bibo:isbn", "isbn10": "bibo:isbn10", "isbn13": "bibo:isbn13",	"oclcnum": "bibo:oclcnum", "issn": "bibo:issn", "issue": "bibo:issue"}')
+	
+	output_file.close()
+	file.close()
 
 if __name__ == "__main__":
 	current_path = os.getcwd()
 	for filename in os.listdir(current_path):
 		if filename.endswith('.jsonld'):
-			#basename = get_basename(filename, current_path)
-
-			#old_filename = current_path+'/'+filename
-			#new_filename = current_path+'/'+basename+'_fixed.jsonld'
-			#copyfile(old_filename, new_filename)
-
-			clean_Files(filename)
+			basename = get_basename(filename, current_path)
+			old_filename = current_path+'/'+filename
+			new_filename = current_path+'/'+basename+'_fixed.jsonld'
+			
+			clean_Files(old_filename, new_filename)
 
