0aa4bdab92aca127d8414c36cae671339ed81968
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-04-26 15:56:07 -0700

    removed unnecessary imports. refactor variable name

--- TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Query.java
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Query.java
@@ -1,19 +1,10 @@
 package org.ECS193.TripleDataProcessor.routes;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response;
 
 import org.ECS193.TripleDataProcessor.data.Data;
 import org.ECS193.TripleDataProcessor.data.EndPoint.ENDPOINT_TYPE;
@@ -25,16 +16,16 @@ public class Query {
 	@POST   
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String post(String input) {
-		System.out.println("POST request: " + input);
+	public String post(String uri) {
+		System.out.println("POST request: " + uri);
 		String result = "";
 		String url = "";
 		JSONObject jsonObject = new JSONObject();
 		try {
-			url = Helper.generate_library_query(input);
+			url = Helper.generate_library_query(uri);
 			result = Helper.query(url);
 			Data data = new Data(result, ENDPOINT_TYPE.library);
-			jsonObject = data.constructJSON(input);
+			jsonObject = data.constructJSON(uri);
 
 		}
 		catch(Exception e) {
