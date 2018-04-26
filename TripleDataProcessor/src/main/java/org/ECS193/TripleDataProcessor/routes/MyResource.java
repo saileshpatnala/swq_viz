4305676c01607e2eef22642b39be81f4fe80d65e
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-04-26 13:00:17 -0700

    fixed syntax issue

--- TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/MyResource.java
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/MyResource.java
@@ -60,7 +60,7 @@ public class MyResource {
 		try {
 			libraryInput = Helper.parserViaf(input);
 			url = Helper.generate_library_query(libraryInput);
-			result = query(url);
+			result = Helper.query(url);
 			Data data = new Data(result, ENDPOINT_TYPE.library);
 			jsonObject = data.constructJSON(input);
 
@@ -71,77 +71,5 @@ public class MyResource {
 	  
 		return jsonObject.toString();
 	}
-	
-
-//	public static String query(String url) throws IOException {
-//		
-//		URL link = new URL(url);
-//		HttpURLConnection httpLink = (HttpURLConnection) link.openConnection();
-//		httpLink.setRequestMethod("GET");
-//
-//		BufferedReader in = new BufferedReader(new InputStreamReader(httpLink.getInputStream()));
-//		String inputLine;
-//		StringBuffer resp = new StringBuffer();
-//		while ((inputLine = in.readLine()) != null) {
-//			resp.append(inputLine);
-//		}
-//
-//		System.out.println("Raw Output: " + resp);
-//		System.out.println();
-//
-//		return resp.toString();
-//	}
-//	
-//	public static String parserViaf(String id) throws IOException {
-//		String libraryInput = "";
-//		String url = generate_query_viaf(id);
-//		String rawJSON = query(url);
-//		
-//		JSONObject temp;
-//		String idElement;
-//		
-//		JSONObject jsonObject = new JSONObject(rawJSON);
-//		JSONArray graphs = jsonObject.getJSONArray("@graph");
-//		
-//		for(int i = 0; i < graphs.length(); i++) {
-//				temp = (JSONObject) graphs.get(i);
-//				idElement = temp.get("@id").toString();
-//				List<String> elements = Arrays.asList(idElement.split("/"));
-//				if(elements.size() == 6) {
-//					String gotIt = elements.get(5);
-//
-//					if (gotIt.substring(0, 2).equals("LC")) {
-//						libraryInput = "n" + gotIt.substring(gotIt.indexOf("++") + 2, gotIt.indexOf("#"));
-//						System.out.println("LIBRARY INPUT: " + libraryInput);
-//						return libraryInput;
-//					}
-//				}
-//		}
-//		
-//		return libraryInput;
-//	}
-//	
-//	public static String generate_query_viaf(String id) {
-//		return "http://viaf.org/viaf/" + id + "/viaf.jsonld";
-//	}
-//	
-//	public static String generate_query(String input) {
-//		// Wiki endpoint
-//		// String url =
-//		// "http://dbpedia.org/sparql?query=SELECT+?subject+?predicate+?object+WHERE+{+?subject+?predicate+?object+}+LIMIT+3&format=json";
-//		// Jena Endpoint
-//		// String url =
-//		// "http://localhost:3030/ds/sparql?query=SELECT+?subject+?predicate+?object+WHERE+{+?subject+?predicate+?object+}+LIMIT+3&format=json";
-//		// VIAF Endpoint
-//		// String url =
-//		// "http://viaf.org/viaf/search?query=cql.any+%3D+%22Chekhov%22&httpAccept=application/json";
-//
-//		// Jena Endpoint for specific search
-//		input = "%22"+input.replaceAll(" ", "%20")+"%22";
-//		String url = 
-//		"http://localhost:3030/ds/sparql?query=select+?subject+?predicate+?object+WHERE+{+?subject+?predicate+?object+.+FILTER+(+REGEX(STR(?subject),+" + input + "+)+%7C%7C+REGEX(STR(?predicate),+"+input+"+)+%7C%7C+REGEX(STR(?object),+"+input+"+)+)+}";
-//
-//		return url;
-//	}
 	
 }
