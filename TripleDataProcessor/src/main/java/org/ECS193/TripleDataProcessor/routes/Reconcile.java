dc4877c3dfa8e773da92e7e5c966ac1b365551e9
Mithun <mithun@vijayasekar.com>
2018-05-06 01:57:44 -0700

    completed reconciler parser

--- TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Reconcile.java
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Reconcile.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -16,6 +18,7 @@ import javax.ws.rs.core.Response;
 
 import org.ECS193.TripleDataProcessor.data.Data;
 import org.ECS193.TripleDataProcessor.data.EndPoint.ENDPOINT_TYPE;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 @Path("reconcile")
@@ -35,18 +38,37 @@ public class Reconcile {
 		}
 	  
 		return output;
-		
 	}
 	
 	public static String parserReconcile(String input) throws IOException {
-		String output = "";
-		String url = Helper.generate_reconcile_query();
+		List<String> possiblePoints = Arrays.asList("viafID", "locID", "wikiID", "author");
+		String url = Helper.generate_reconciler_query(input);
 		String rawJSON = Helper.query(url);
-		
-		
+		JSONObject jsonOut = new JSONObject();
+		JSONArray array = new JSONArray();
+
+		//Parsing
 		JSONObject jsonObject = new JSONObject(rawJSON);
 		
-		return "";
+		JSONObject headObject = jsonObject.getJSONObject("head");
+        JSONArray endPoints = headObject.getJSONArray("vars");
+        
+		JSONObject resultObject = jsonObject.getJSONObject("results");
+        JSONArray bindingPoints = resultObject.getJSONArray("bindings");
+        
+        for(int i = 0; i < endPoints.length(); i++) {
+            	
+        		if(possiblePoints.contains(endPoints.get(i))) {
+        			JSONObject temp = new JSONObject();
+        			String key = endPoints.get(i).toString();
+        			String value = bindingPoints.getJSONObject(i).get("value").toString();
+        			temp.put(key, value);
+        			array.put(temp);
+        		}
+        }
+        jsonOut.put("Reconciler", array);
+
+		return jsonOut.toString();
 	}
 	
 }
