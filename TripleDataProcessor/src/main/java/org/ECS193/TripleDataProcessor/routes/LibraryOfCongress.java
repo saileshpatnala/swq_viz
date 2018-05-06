69f593fab1357c47e9bc1f774c779ecebff0e71b
Mithun <mithun@vijayasekar.com>
2018-05-06 00:57:38 -0700

    setup library of congress post request

---
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/LibraryOfCongress.java
@@ -0,0 +1,53 @@
+package org.ECS193.TripleDataProcessor.routes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.ECS193.TripleDataProcessor.data.Data;
+import org.ECS193.TripleDataProcessor.data.EndPoint.ENDPOINT_TYPE;
+import org.json.JSONObject;
+
+@Path("libraryofcongress")
+public class LibraryOfCongress {
+
+	@POST   
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String post(String input) {
+		System.out.println("POST request: " + input);
+		String output = "";
+		try {
+			output = parserOCLC(input);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	  
+		return output;
+		
+	}
+	
+	public static String parserLC(String input) throws IOException {
+		String output = "";
+		String url = Helper.generate_oclc_query(input);
+		String rawJSON = Helper.query(url);
+		
+		
+		JSONObject jsonObject = new JSONObject(rawJSON);
+		
+		
+		return "";
+	}
+	
+}
