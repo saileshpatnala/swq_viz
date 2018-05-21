c3fba49620a7f4dfa20845ab1336317755846554
wonheep <wonheeprk@gmail.com>
2018-05-20 18:49:22 -0700

    finish dbpedia parser and endpoint

---
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/DBpedia.java
@@ -0,0 +1,56 @@
+package org.ECS193.TripleDataProcessor.routes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.ECS193.TripleDataProcessor.data.Data;
+import org.ECS193.TripleDataProcessor.data.EndPoint.ENDPOINT_TYPE;
+import org.json.JSONObject;
+
+@Path("dbpedia")
+public class DBpedia {
+
+	@POST   
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String post(String input) {
+		System.out.println("POST request: " + input);
+		String output = "";
+
+		try {
+			return parserDBpedia(input);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+	
+	public static String parserDBpedia(String input) throws IOException {
+		String output = "";
+		JSONObject jsonObject = new JSONObject();
+
+		try {
+			String url = Helper.generate_dbpedia_query(input);
+			String rawJSON = Helper.query(url);
+			
+			Data data = new Data(rawJSON, ENDPOINT_TYPE.dbpedia);
+			jsonObject = data.constructJSON(input);		
+		}	
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return jsonObject.toString();
+	}
+}
--- TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Helper.java
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Helper.java
@@ -19,6 +19,7 @@ import org.json.JSONObject;
 		- Wiki/DBPedia - metadata
 		- DBPedia 
 		- UC Davis Library Catalog
+		- FAST and ISNI DB won't be queried b/c data not returned in acceptable format
  */
 public class Helper {
 	
@@ -40,6 +41,7 @@ public class Helper {
 		return resp.toString();
 	}
 	
+
 	/* NOTE:
 		- Generates JSON list of reconciler IDs and author
 		- Query on the LOC ID 
@@ -50,17 +52,20 @@ public class Helper {
 		return "http://query.projectpassage.org/sparql/?query=PREFIX%20passagedt%3A%20%3Chttp%3A%2F%2F18.218.102.193%2Fprop%2Fdirect%2F%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20passagee%3A%20%3Chttp%3A%2F%2F18.218.102.193%2Fentity%2F%3E%0APREFIX%20wikibase%3A%20%3Chttp%3A%2F%2Fwikiba.se%2Fontology%23%3E%0APREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0A%0ASELECT%20%3FpersonURL%20%3FviafID%20%3FlocID%20%3FwikiID%20%3FfastID%20%3FisniID%20%3FimdbID%20%3Fauthor%20WHERE%20%7B%3FpersonURL%20passagedt%3AP5%20passagee%3AQ7%3B%0A%20passagedt%3AP6%20%3FviafID%3B%20passagedt%3AP105%20%3FlocID%3B%20passagedt%3AP8%20%3FwikiID%3B%20passagedt%3AP7%20%3FfastID%3B%20passagedt%3AP40%20%3FisniID%3B%20passagedt%3AP116%20%3FimdbID%3B%20rdfs%3Alabel%20%3Fauthor.%0A%20FILTER%28LANG%28%3Fauthor%29%3D%22en%22%20%26%26%20REGEX%28STR%28%3FlocID%29%2C%22"+loc_id+"%22%29%29.%0A%7D&format=json";
 	}
 	
+
 	/* VIAF */
 	public static String generate_viaf_query(String id) {
 		id = id.replaceAll(" ", "%20"); 
 		return "http://viaf.org/viaf/" + id + "/viaf.jsonld";
 	}
+
 	
 	/* WikiData - metadata */
 	public static String generate_wiki_query(String id) {
 		id = id.replaceAll(" ", "%20"); 
 		return "https://www.wikidata.org/wiki/Special:EntityData/" + id + ".nt"; 
 	}
+
 
 	/* Wiki/DBpedia - metadata */
 	public static String generate_wikidbpedia_query(String id) {
@@ -69,17 +74,50 @@ public class Helper {
 
 	}
 	
+
 	/* DBpedia */
-	public static String generate_dbpedia_query(String id) {
+	public static String generate_dbpedia_query(String id) throws IOException {
 		/* STEP 1 - get foaf:name literal from wiki ID */
 		id = id.replaceAll(" ", "%20");
-		var dbpQuery = "http://dbpedia.org/sparql?query=PREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fproperty%2F%3E%0D%0ASELECT+%3Fperson+WHERE+%7B%3Fperson+dbp%3AdSearch+%3Fid+.+FILTER%28REGEX%28STR%28%3Fid%29%2C+%22" + id + "%22%29%29%7D+&format=application%2Fsparql-results%2Bjson";
+		String dbpQuery = "http://dbpedia.org/sparql?query=PREFIX+dbp%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fproperty%2F%3E%0D%0ASELECT+%3Fperson+WHERE+%7B%3Fperson+dbp%3AdSearch+%3Fid+.+FILTER%28REGEX%28STR%28%3Fid%29%2C+%22" + id + "%22%29%29%7D+&format=application%2Fsparql-results%2Bjson";
 
-		var name = ;
+		String rawJSON = query(dbpQuery);
+		
+		JSONObject jsonObject = new JSONObject(rawJSON);
+		JSONArray bindings = jsonObject.getJSONObject("results").getJSONArray("bindings");
+
+		JSONObject temp = (JSONObject) bindings.get(0);
+		JSONObject temp2 = (JSONObject) temp.get("person");
+		String idElement = temp2.get("value").toString();
+		List<String> elements = Arrays.asList(idElement.split("/"));
+		String name = elements.get(4);
+		System.out.println("---- name from DBpedia ------ " + name);
+
+		// {
+		//   "head": {
+		//     "link": [],
+		//     "vars": [
+		//       "person"
+		//     ]
+		//   },
+		//   "results": {
+		//     "distinct": false,
+		//     "ordered": true,
+		//     "bindings": [
+		//       {
+		//         "person": {
+		//           "type": "uri",
+		//           "value": "http://dbpedia.org/resource/Jane_Austen"
+		//         }
+		//       }
+		//     ]
+		//   }
+		// }
 
 		/* STEP 2 - get n-triples dataset from foaf:name literal */
 		return "dbpedia.org/data/" + name + ".ntriples ";
 	}
+
 
 	/* LOC */
 	public static String generate_lc_query(String id) {
@@ -87,6 +125,7 @@ public class Helper {
 		return "http://id.loc.gov/authorities/names/" + id + ".nt";
 	}
 	
+
 	/* OCLC */	
 	/* NOTE:
 	 	- Worldcat.org/oclc ID not given from reconciler
@@ -97,6 +136,7 @@ public class Helper {
 		return "http://www.worldcat.org/oclc/" + id + ".nt";
 	}
 	
+
 	/* OMDB (+IMDB) */
 	/* NOTE:
 		- OMDB API requires a key, current = 10729f07.
@@ -109,6 +149,7 @@ public class Helper {
 		return "http://www.omdbapi.com/?i=" + id + "&plot=full&r=json&apikey=10729f07";
 	}
 	
+
 	/* UC Davis Library Catalog */ 
 	/* NOTE:
 		- initial process, query on: LOC ID -> LIBRARY ID -> URIs
@@ -120,6 +161,7 @@ public class Helper {
 
 		return url;
 	}
+
 	
 	/* NOTE: 
 		- Parse JSON array object returned from VIAF DB query
@@ -154,12 +196,7 @@ public class Helper {
 		return libraryInput;
 	}
 	
-	/* NOTE: 
-		- Returning data in n-triples
-		- FAST and ISNI DB will not be queried b/c data isn't returned in acceptable format. 
-		- "http://id.worldcat.org/fast/" + id;
-		- "http://www.isni.org/isni/" + id;
-	 */
+
 	// @MITHUN - delete if not planning to use later
 	public static String generate_URI_query(String uri, String type) {
 		String id = uri.replaceAll(" ", "%20"); 
