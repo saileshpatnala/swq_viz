3c0e01bb7ceff9dffce0bb8b21dc21b2c7ec5337
wonheep <wonheeprk@gmail.com>
2018-06-03 20:33:34 -0700

    fixed back button, and debugged alignment of images in about us page

---
+++ TripleDataProcessor/src/main/java/org/ECS193/TripleDataProcessor/routes/Contact.java
@@ -0,0 +1,30 @@
+package org.ECS193.TripleDataProcessor.routes;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+@Path("contact")
+public class Contact {
+	
+	private String searchFilePath = "src/main/webapp/contact.html";
+
+	@GET
+	@Produces({MediaType.TEXT_HTML})
+	public InputStream viewSearch()
+	{
+		try {
+			File f = new File(this.searchFilePath);
+			return new FileInputStream(f);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
