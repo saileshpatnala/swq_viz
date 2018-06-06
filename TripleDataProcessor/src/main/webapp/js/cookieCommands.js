a2515ff9fa9ab843a7659e0df9c7f1dcd09c1e17
NishantHome <nchandrashekar@ucdavis.edu>
2018-06-06 16:45:13 -0700

    got search term to show up

--- TripleDataProcessor/src/main/webapp/js/cookieCommands.js
+++ TripleDataProcessor/src/main/webapp/js/cookieCommands.js
@@ -1,14 +1,18 @@
-function setCookie(value) {
+
+// expiry set for 1 day
+function SetCookie(name, value){
     var expires = "";
-    // if (days) {
+    delete_cookie(name);
+    if (true) {
         var date = new Date();
-        date.setTime(date.getTime() + (9999999999));
+        date.setTime(date.getTime() + (1*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
-    // }
-    document.cookie = "InputString=" + (value || "")  + expires + "; path=/";
+    }
+    document.cookie = name + "=" + value + ";" + expires + ";";
 }
-function getCookie() {
-    var nameEQ = "InputString=";
+
+function GetCookie(name) {
+    var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
         var c = ca[i];
@@ -18,6 +22,6 @@ function getCookie() {
     return null;
 }
 
-function eraseCookie() {   
-    document.cookie = 'InputString=; Max-Age=-99999999;';  
+function delete_cookie( name ) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
